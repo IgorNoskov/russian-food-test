91e2df0a1cde0b185ba233c1f1e528bf72bc8d98
IgorNoskov <i.noskov@alef.im>
2020-02-08 23:34:17 +0700

    Add tests for ./data/quizData.js

---
+++ src/tests/data/quizData.test.js
@@ -0,0 +1,35 @@
+import quizData from '../../data/quizData';
+
+describe('quizData has right structure', () => {
+    test('quizData has question array', () => {
+        expect(Array.isArray(quizData.questions)).toBe(true);
+    });
+
+    test('questions has id and id is number', () => {
+        quizData.questions.forEach((question) => {
+            expect(question.hasOwnProperty('id')).toBe(true);
+            expect(typeof question.id === 'number').toBe(true);
+        });
+    });
+
+    test('questions has question and question is string', () => {
+        quizData.questions.forEach((question) => {
+            expect(question.hasOwnProperty('question')).toBe(true);
+            expect(typeof question.question === 'string').toBe(true);
+        });
+    });
+
+    test('questions has options and options are arrays', () => {
+        quizData.questions.forEach((question) => {
+            expect(question.hasOwnProperty('options')).toBe(true);
+            expect(Array.isArray(question.options)).toBe(true);
+        });
+    });
+
+    test('questions has correctAnswer', () => {
+        quizData.questions.forEach((question) => {
+            expect(question.hasOwnProperty('correctAnswer')).toBe(true);
+            expect(typeof question.question === 'string').toBe(true);
+        });
+    });
+});
